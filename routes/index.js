81fb7d096b04729189736c2070fbc2ee0474bb26
Sinda <sinda.gouret@gmail.com>
2024-04-12 12:14:20 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/year', function(req, res) {
+  const date = new Date();
+  const year = date.getFullYear();
+  console.log(year)
+  res.json({ year:year });
+});
+
+module.exports = router;

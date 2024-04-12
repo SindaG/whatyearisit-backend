ec8ee0754dd07c1bc90663f5a42790fd8a8614f4
Sinda <sinda.gouret@gmail.com>
2024-04-12 13:56:22 +0200

    correction url

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/year', function(req, res) {
   const date = new Date();
-  const year = date.getFullYear();
+  const year = date.getFullYear().toString();
   console.log(year)
   res.json({ year:year });
 });
